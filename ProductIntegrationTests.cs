d45f16b52cca654868173ecc4b6830149ed6c0cc
AyoubSaghir <ayoub.saghir@zupit.it>
2023-09-25 14:24:22 +0200

    Added test case for api get products by filter

--- ProductIntegrationTests.cs
+++ ProductIntegrationTests.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using System.Text.Json;
 using Ecommerce.Models;
+using Ecommerce.Filters;
 
 namespace EF_Core_Project_Ecommerce.Tests
 {
@@ -37,6 +38,31 @@ namespace EF_Core_Project_Ecommerce.Tests
         {
             var response = await _client.GetAsync("/products/category/electronics");
             response.EnsureSuccessStatusCode();
+        }
+        [Fact]
+        public async Task GetAllProductsByFilter_ReturnsSuccessStatusCode()
+        {
+            var productFilter = new ProductFilter
+            {
+                Slug = "electronics",
+                Min = 100,
+                Max = 1000
+            };
+            
+            var productFilterJson = JsonSerializer.Serialize(productFilter);
+            var productFilterContent = new StringContent(productFilterJson, Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync("/products/filter/", productFilterContent);
+
+            response.EnsureSuccessStatusCode();
+            
+            var products = JsonSerializer.Deserialize<List<Product>>(
+                await response.Content.ReadAsStringAsync(),
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+            
+            Assert.NotNull(products);
+            Assert.NotEmpty(products);
         }
 
         public void Dispose()
